26297093504f843227f0a81592fa45942f50a84e
Brady <zeromemesdev@gmail.com>
2022-12-22 20:16:54 -0600

    Partial constexpr wrapper support

--- include/libhat/Callable.hpp
+++ include/libhat/Callable.hpp
@@ -21,7 +21,7 @@ namespace hat::detail {
 
         template<supplier<Ret> Capture>
         struct context {
-            Ret operator()(Args&& ... args) const {
+            Ret operator()(Args&&... args) const {
                 return original(std::forward<Args>(args)...);
             }
 
@@ -30,7 +30,6 @@ namespace hat::detail {
             }
 
             context(function_type* original, Capture&& captured) : original(original), captured(captured) {}
-
         private:
             function_type* original;
             Capture captured;
@@ -39,8 +38,10 @@ namespace hat::detail {
         template<typename T>
         context(function_type*, T&&) -> context<T>;
 
-        template<typename Wrapper, auto get_original>
+        template<auto wrapper, auto get_original>
         struct caller {
+            using wrapper_type = decltype(wrapper);
+
             static Ret invoke(Args... args) {
                 const auto original = get_original();
                 const context ctx{
@@ -49,62 +50,75 @@ namespace hat::detail {
                 };
                 using ctx_t = decltype(ctx);
 
-                constexpr bool invoke_with_args = std::is_invocable_v<Wrapper, const ctx_t&, Args&& ...>;
+                constexpr bool invoke_with_args = std::is_invocable_v<wrapper_type, const ctx_t&, Args&&...>;
 
                 if constexpr (invoke_with_args) {
                     // If the wrapper wants to receive the function arguments
-                    static_assert(std::is_same_v<Ret, std::invoke_result_t<Wrapper, const ctx_t&, Args&& ...>>);
-                    return Wrapper{}(ctx, std::forward<decltype(args)>(args)...);
+                    static_assert(std::is_same_v<Ret, std::invoke_result_t<wrapper_type, const ctx_t&, Args&&...>>);
+                    return wrapper(ctx, std::forward<decltype(args)>(args)...);
                 } else {
                     // Otherwise don't bother
-                    static_assert(std::is_same_v<Ret, std::invoke_result_t<Wrapper, const ctx_t&>>);
-                    return Wrapper{}(ctx);
+                    static_assert(std::is_same_v<Ret, std::invoke_result_t<wrapper_type, const ctx_t&>>);
+                    return wrapper(ctx);
                 }
             };
         };
 
-        template<typename Provider>
-        struct capturing_static_storage {
-            alignas(Provider) static inline std::byte impl[sizeof(Provider)]{};
-
+        template<supplier<function_type*> Provider>
+        struct provider_storage {
             static inline function_type* get_original() {
-                return (*std::launder(reinterpret_cast<Provider*>(impl)))();
+                return (*std::launder(reinterpret_cast<Provider*>(instance)))();
             }
+
+            static inline void set_provider(Provider&& provider) {
+                new (instance) Provider{std::move(provider)};
+            }
+        private:
+            alignas(Provider) static inline std::byte instance[sizeof(Provider)]{};
         };
     };
 }
 
 namespace hat {
 
-    template<detail::function Fn, std::default_initializable Wrapper>
-    auto make_dynamic_wrapper(Wrapper, detail::supplier<Fn*> auto wrapped) -> Fn* {
+    template<
+        detail::function           Fn,
+        std::default_initializable Wrapper,
+        detail::supplier<Fn*>      Wrapped
+    > requires std::default_initializable<Wrapped>
+    constexpr auto make_dynamic_wrapper(Wrapper, Wrapped) -> Fn* {
+        return detail::wrapper_util<Fn>::template caller<Wrapper{}, Wrapped{}>::invoke;
+    }
+
+    template<
+        detail::function           Fn,
+        std::default_initializable Wrapper,
+        detail::supplier<Fn*>      Wrapped
+    > requires (!std::default_initializable<Wrapped>)
+    auto make_dynamic_wrapper(Wrapper, Wrapped wrapped) -> Fn* {
         using util_t = detail::wrapper_util<Fn>;
-        if constexpr (std::is_default_constructible_v<decltype(wrapped)>) {
-            return util_t::template caller<Wrapper, decltype(wrapped){}>::invoke;
-        } else {
-            const auto provider = [wrapped = std::move(wrapped)]() -> Fn* {
-                return wrapped();
-            };
+        const auto provider = [wrapped = std::move(wrapped)]() -> Fn* {
+            return wrapped();
+        };
 
-            using storage = util_t::template capturing_static_storage<decltype(provider)>;
-            new (storage::impl) decltype(provider){std::move(provider)};
+        using storage = util_t::template provider_storage<decltype(provider)>;
+        storage::set_provider(std::move(provider));
 
-            return util_t::template caller<Wrapper, storage::get_original>::invoke;
-        }
+        return util_t::template caller<Wrapper{}, storage::get_original>::invoke;
     }
 
     template<auto wrapped, detail::function Fn = std::remove_pointer_t<decltype(wrapped)>>
-    auto make_static_wrapper(auto wrapper) {
+    constexpr auto make_static_wrapper(std::default_initializable auto wrapper) {
         return make_dynamic_wrapper<Fn>(wrapper, []() { return wrapped; });
     }
 
     template<detail::function Fn>
-    auto make_static_wrapper(auto wrapper, Fn* wrapped) {
+    auto make_static_wrapper(std::default_initializable auto wrapper, Fn* wrapped) {
         return make_dynamic_wrapper<Fn>(wrapper, [=]() { return wrapped; });
     }
 
     template<typename Ret, typename... Args>
-    auto make_static_wrapper(auto wrapper, Ret(* wrapped)(Args...)) {
+    auto make_static_wrapper(std::default_initializable auto wrapper, Ret(*wrapped)(Args...)) {
         return make_static_wrapper<Ret(Args...)>(wrapper, wrapped);
     }
 }
