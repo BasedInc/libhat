79843adaca6876eb0f753525d6b501c9f2cb04e9
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2023-12-19 16:04:49 -0500

    Fix horrific code practices, what a shame

--- src/os/win32/MemoryProtector.cpp
+++ src/os/win32/MemoryProtector.cpp
@@ -6,10 +6,10 @@
 #include <Windows.h>
 
 namespace hat {
-    static DWORD ToWinProt(protection flags) {
-        const bool r = bool(flags & protection::Read);
-        const bool w = bool(flags & protection::Write);
-        const bool x = bool(flags & protection::Execute);
+    static DWORD ToWinProt(const protection flags) {
+        const bool r = static_cast<bool>(flags & protection::Read);
+        const bool w = static_cast<bool>(flags & protection::Write);
+        const bool x = static_cast<bool>(flags & protection::Execute);
 
         if (x && w) return PAGE_EXECUTE_READWRITE;
         if (x && r) return PAGE_EXECUTE_READ;
@@ -19,7 +19,7 @@ namespace hat {
         return PAGE_NOACCESS;
     }
 
-    memory_protector::memory_protector(uintptr_t address, size_t size, protection flags) : address(address), size(size) {
+    memory_protector::memory_protector(const uintptr_t address, const size_t size, const protection flags) : address(address), size(size) {
         VirtualProtect(reinterpret_cast<LPVOID>(this->address), this->size, ToWinProt(flags), reinterpret_cast<PDWORD>(&this->oldProtection));
     }
 
