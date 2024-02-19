1b83b822be6fd42a0137c89445eb6c9ac5373daa
Brady <zeromemesdev@gmail.com>
2024-02-18 21:54:42 -0600

    UDLs for string_literal

--- include/libhat/StringLiteral.hpp
+++ include/libhat/StringLiteral.hpp
@@ -24,3 +24,31 @@ namespace hat {
     template<hat::u32fixed_string str>
     using u32string_literal = basic_string_literal<str>;
 }
+
+namespace hat::literals::string_literals {
+
+    template<hat::fixed_string str>
+    consteval auto operator""_s() noexcept {
+        return string_literal<str>();
+    }
+
+    template<hat::wfixed_string str>
+    consteval auto operator""_ws() noexcept {
+        return wstring_literal<str>();
+    }
+
+    template<hat::u8fixed_string str>
+    consteval auto operator""_u8s() noexcept {
+        return u8string_literal<str>();
+    }
+
+    template<hat::u16fixed_string str>
+    consteval auto operator""_u16s() noexcept {
+        return u16string_literal<str>();
+    }
+
+    template<hat::u32fixed_string str>
+    consteval auto operator""_u32s() noexcept {
+        return u32string_literal<str>();
+    }
+}
