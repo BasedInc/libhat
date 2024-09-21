87c336c8ac00342ac589a2d7519a7b456bbbae7f
Brady <zeromemesdev@gmail.com>
2024-09-21 15:42:15 -0500

    Fix missing header include

--- include/libhat/Access.hpp
+++ include/libhat/Access.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdint>
 #include <concepts>
 
 #include "Traits.hpp"
@@ -9,6 +10,6 @@ namespace hat {
     template<typename MemberType, typename Base>
     auto& member_at(Base* ptr, std::integral auto offset) {
         using Ret = hat::constness_as_t<MemberType, Base>;
-        return *reinterpret_cast<Ret*>(reinterpret_cast<uintptr_t>(ptr) + offset);
+        return *reinterpret_cast<Ret*>(reinterpret_cast<std::uintptr_t>(ptr) + offset);
     }
 }
