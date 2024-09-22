5a7970877b297b236818ee2d71730f4f1ca1a06c
Brady <zeromemesdev@gmail.com>
2024-09-21 19:10:51 -0500

    Fix conversion warning

--- include/libhat/CompileTime.hpp
+++ include/libhat/CompileTime.hpp
@@ -14,15 +14,15 @@ namespace hat {
     };
 
     template<std::integral Integer, detail::char_iterator Iter>
-    inline constexpr result<Integer, parse_int_error> parse_int(Iter begin, Iter end, int base = 10) noexcept {
+    inline constexpr result<Integer, parse_int_error> parse_int(Iter begin, Iter end, uint8_t base = 10) noexcept {
         if (base < 2 || base > 36) {
             return result_error{parse_int_error::invalid_base};
         }
 
         Integer sign = 1;
         Integer value = 0;
-        const auto digits = base < 10 ? base : 10;
-        const auto letters = base > 10 ? base - 10 : 0;
+        const int digits = base < 10 ? base : 10;
+        const int letters = base > 10 ? base - 10 : 0;
 
         for (auto iter = begin; iter != end; iter++) {
             const char ch = *iter;
@@ -54,7 +54,7 @@ namespace hat {
     }
 
     template<typename Integer>
-    inline constexpr result<Integer, parse_int_error> parse_int(std::string_view str, int base = 10) noexcept {
+    inline constexpr result<Integer, parse_int_error> parse_int(std::string_view str, uint8_t base = 10) noexcept {
         return parse_int<Integer>(str.cbegin(), str.cend(), base);
     }
 }
