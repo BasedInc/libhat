31f4936df6399a116ac66580d6b5f2f4d7321879
Brady <zeromemesdev@gmail.com>
2024-09-30 15:22:55 -0500

    Fix inclusion of x86-specific header

--- include/libhat/Defines.hpp
+++ include/libhat/Defines.hpp
@@ -47,14 +47,8 @@
     #define LIBHAT_RETURN_ADDRESS() _ReturnAddress()
     #define LIBHAT_BSF32(num) hat::detail::bsf(num)
 #else
-    #if defined(__clang__)
-        #include <x86intrin.h>
-    #elif defined(__GNUC__)
-        #include <x86gprintrin.h>
-    #endif
-
     #define LIBHAT_RETURN_ADDRESS() __builtin_extract_return_addr(__builtin_return_address(0))
-    #define LIBHAT_BSF32(num) _bit_scan_forward(num)
+    #define LIBHAT_BSF32(num) __builtin_ctz(num)
 #endif
 
 #if __cpp_if_consteval >= 202106L
