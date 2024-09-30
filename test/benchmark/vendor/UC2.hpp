337ce1ef1c4b74a8fbdefde91ca3bdd64ba51040
Brady <zeromemesdev@gmail.com>
2024-09-30 13:29:58 -0500

    Replace tabs with 4 spaces

--- test/benchmark/vendor/UC2.hpp
+++ test/benchmark/vendor/UC2.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#define INRANGE(x,a,b)	(x >= a && x <= b)
-#define getBits( x )	(INRANGE((x&(~0x20)),'A','F') ? ((x&(~0x20)) - 'A' + 0xa) : (INRANGE(x,'0','9') ? x - '0' : 0))
-#define getByte( x )	(getBits(x[0]) << 4 | getBits(x[1]))
+#define INRANGE(x,a,b)  (x >= a && x <= b)
+#define getBits( x )    (INRANGE((x&(~0x20)),'A','F') ? ((x&(~0x20)) - 'A' + 0xa) : (INRANGE(x,'0','9') ? x - '0' : 0))
+#define getByte( x )    (getBits(x[0]) << 4 | getBits(x[1]))
 
 // https://www.unknowncheats.me/forum/650040-post8.html
 namespace UC2 {
