f2c12328ef6e6ad288d36a9cd7516cf8fe2d913b
Brady <zeromemesdev@gmail.com>
2025-03-14 15:19:36 -0700

    Fix `noexcept` specifiers on `hat::result`

--- include/libhat/result.hpp
+++ include/libhat/result.hpp
@@ -18,12 +18,14 @@ namespace hat {
     class result_error {
         template<typename T, typename R>
         friend class result;
-    public:
-        constexpr explicit result_error(const E& t) : error(t) {}
-        constexpr explicit result_error(E&& t) : error(std::move(t)) {}
 
-        constexpr result_error(const result_error<E>&) = default;
-        constexpr result_error(result_error<E>&&) noexcept = default;
+    public:
+        constexpr explicit result_error(const E& t) noexcept(std::is_nothrow_copy_constructible_v<E>)
+            : error(t) {}
+
+        constexpr explicit result_error(E&& t) noexcept(std::is_nothrow_move_constructible_v<E>)
+            : error(std::move(t)) {}
+
     private:
         E error;
     };
@@ -32,25 +34,55 @@ namespace hat {
     /// If available, std::expected is used as the underlying implementation. Otherwise, std::variant is used.
     template<class T, class E>
     class result {
+#ifdef LIBHAT_RESULT_EXPECTED
+        using underlying_type = std::expected<T, E>;
+#else
+        using underlying_type = std::variant<T, E>;
+#endif
+        underlying_type impl;
+
     public:
 #ifdef LIBHAT_RESULT_EXPECTED
-        LIBHAT_CONSTEXPR_RESULT result(const T& t) : impl(std::in_place, t) {}
-        LIBHAT_CONSTEXPR_RESULT result(T&& t) : impl(std::in_place, std::move(t)) {}
+        LIBHAT_CONSTEXPR_RESULT result(const T& t) noexcept(
+            std::is_nothrow_constructible_v<underlying_type, std::in_place_t, const T&>)
+            : impl(std::in_place, t) {}
 
-        LIBHAT_CONSTEXPR_RESULT result(const result_error<E>& e) : impl(std::unexpect, e.error) {}
-        LIBHAT_CONSTEXPR_RESULT result(result_error<E>&& e) : impl(std::unexpect, std::move(e.error)) {}
+        LIBHAT_CONSTEXPR_RESULT result(T&& t) noexcept(
+            std::is_nothrow_constructible_v<underlying_type, std::in_place_t, T&&>)
+            : impl(std::in_place, std::move(t)) {}
+
+        LIBHAT_CONSTEXPR_RESULT result(const result_error<E>& e) noexcept(
+            std::is_nothrow_constructible_v<underlying_type, std::unexpect_t, const result_error<E>&>)
+            : impl(std::unexpect, e.error) {}
+
+        LIBHAT_CONSTEXPR_RESULT result(result_error<E>&& e) noexcept(
+            std::is_nothrow_constructible_v<underlying_type, std::unexpect_t, result_error<E>&&>)
+            : impl(std::unexpect, std::move(e.error)) {}
 #else
-        LIBHAT_CONSTEXPR_RESULT result(const T& t) : impl(std::in_place_index<0>, t) {}
-        LIBHAT_CONSTEXPR_RESULT result(T&& t) : impl(std::in_place_index<0>, std::move(t)) {}
+        LIBHAT_CONSTEXPR_RESULT result(const T& t) noexcept(
+            std::is_nothrow_constructible_v<underlying_type, std::in_place_index_t<0>, const T&>)
+            : impl(std::in_place_index<0>, t) {}
 
-        LIBHAT_CONSTEXPR_RESULT result(const result_error<E>& e) : impl(std::in_place_index<1>, e.error) {}
-        LIBHAT_CONSTEXPR_RESULT result(result_error<E>&& e) : impl(std::in_place_index<1>, std::move(e.error)) {}
+        LIBHAT_CONSTEXPR_RESULT result(T&& t) noexcept(
+            std::is_nothrow_constructible_v<underlying_type, std::in_place_index_t<0>, T&&>)
+            : impl(std::in_place_index<0>, std::move(t)) {}
+
+        LIBHAT_CONSTEXPR_RESULT result(const result_error<E>& e) noexcept(
+            std::is_nothrow_constructible_v<underlying_type, std::in_place_index_t<1>, const result_error<E>&>)
+            : impl(std::in_place_index<1>, e.error) {}
+
+        LIBHAT_CONSTEXPR_RESULT result(result_error<E>&& e) noexcept(
+            std::is_nothrow_constructible_v<underlying_type, std::in_place_index_t<1>, result_error<E>&&>)
+            : impl(std::in_place_index<1>, std::move(e.error)) {}
 #endif
 
-        LIBHAT_CONSTEXPR_RESULT result(const result<T, E>&) = default;
-        LIBHAT_CONSTEXPR_RESULT result(result<T, E>&&) noexcept = default;
+        LIBHAT_CONSTEXPR_RESULT result(const result<T, E>&) noexcept(
+            std::is_nothrow_copy_constructible_v<underlying_type>) = default;
 
-        [[nodiscard]] LIBHAT_CONSTEXPR_RESULT bool has_value() const {
+        LIBHAT_CONSTEXPR_RESULT result(result<T, E>&&) noexcept(
+            std::is_nothrow_move_constructible_v<underlying_type>) = default;
+
+        [[nodiscard]] LIBHAT_CONSTEXPR_RESULT bool has_value() const noexcept {
 #ifdef LIBHAT_RESULT_EXPECTED
             return impl.has_value();
 #else
@@ -58,7 +90,7 @@ namespace hat {
 #endif
         }
 
-        LIBHAT_CONSTEXPR_RESULT T& value() noexcept {
+        LIBHAT_CONSTEXPR_RESULT T& value() {
 #ifdef LIBHAT_RESULT_EXPECTED
             return impl.value();
 #else
@@ -66,7 +98,7 @@ namespace hat {
 #endif
         }
 
-        LIBHAT_CONSTEXPR_RESULT const T& value() const noexcept {
+        LIBHAT_CONSTEXPR_RESULT const T& value() const {
 #ifdef LIBHAT_RESULT_EXPECTED
             return impl.value();
 #else
@@ -74,7 +106,7 @@ namespace hat {
 #endif
         }
 
-        LIBHAT_CONSTEXPR_RESULT E& error() noexcept {
+        LIBHAT_CONSTEXPR_RESULT E& error() {
 #ifdef LIBHAT_RESULT_EXPECTED
             return impl.error();
 #else
@@ -82,20 +114,13 @@ namespace hat {
 #endif
         }
 
-        LIBHAT_CONSTEXPR_RESULT const E& error() const noexcept {
+        LIBHAT_CONSTEXPR_RESULT const E& error() const {
 #ifdef LIBHAT_RESULT_EXPECTED
             return impl.error();
 #else
             return std::get<1>(impl);
 #endif
         }
-
-    private:
-#ifdef LIBHAT_RESULT_EXPECTED
-        std::expected<T, E> impl;
-#else
-        std::variant<T, E> impl;
-#endif
     };
 }
 
