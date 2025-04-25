a2f5fdc76f051a2b6f9d0e8a3fcf1c67c9162377
Brady <zeromemesdev@gmail.com>
2025-04-24 22:23:52 -0500

    *whoops*

--- src/os/linux/Common.hpp
+++ src/os/linux/Common.hpp
@@ -10,7 +10,7 @@
 
 #include <sys/mman.h>
 
-static void iter_mapped_regions(const uintptr_t address, std::predicate<uintptr_t, uintptr_t, uint32_t> auto&& callback) {
+static void iter_mapped_regions(std::predicate<uintptr_t, uintptr_t, uint32_t> auto&& callback) {
     std::ifstream f("/proc/self/maps");
     std::string s;
     while (std::getline(f, s)) {
@@ -33,7 +33,7 @@ static void iter_mapped_regions(const uintptr_t address, std::predicate<uintptr_
         it = res.ptr + 1; // +1 to skip the space
 
         std::string_view remaining{it, end};
-        if (address >= pageBegin && address < pageEnd && remaining.size() >= 3) {
+        if (remaining.size() >= 3) {
             uint32_t prot = 0;
             if (remaining[0] == 'r') prot |= PROT_READ;
             if (remaining[1] == 'w') prot |= PROT_WRITE;
@@ -47,7 +47,7 @@ static void iter_mapped_regions(const uintptr_t address, std::predicate<uintptr_
 
 static std::optional<uint32_t> get_page_prot(const uintptr_t address) {
     std::optional<uint32_t> result;
-    iter_mapped_regions(address, [&](const uintptr_t begin, const uintptr_t end, uint32_t prot) {
+    iter_mapped_regions([&](const uintptr_t begin, const uintptr_t end, uint32_t prot) {
         if (address >= begin && address < end) {
             result = prot;
             return false;
